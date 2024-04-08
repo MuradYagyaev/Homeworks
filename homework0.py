7cfe1380274decf2d471db8da8913f72e9739a7d
MuradYagyaev <165575932+MuradYagyaev@users.noreply.github.com>
2024-04-08 22:51:29 +0300

    Add files via upload

---
+++ homework0.py
@@ -0,0 +1,11 @@
+text = 'это моя строка'
+print(text[0])
+print(text[-1])
+print(text[2:5])
+print(text[::-1])
+print(len(text))
+print('это новая строка ' + text)
+# Далее решение: сначала создаю новую строковую переменную, а потом вывоэу ее на экран
+text2 = 'это новая строка'
+text3 = text2 + ' ' + text
+print(text3)
