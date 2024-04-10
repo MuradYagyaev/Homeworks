758d3af0cdd1b81a9f8d67d51e22cb52a0d6e799
MuradYagyaev <165575932+MuradYagyaev@users.noreply.github.com>
2024-04-10 21:27:35 +0300

    Delete homework3.py

--- homework3.py
+++
@@ -1,8 +0,0 @@
-# Модуль 1 урок №4. "Организация программ и методы строк."
-my_string = input()
-print("Длина строки:", len(my_string), "символов")
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(' ', ''))
-print(my_string[0])
-print(my_string[-1])
