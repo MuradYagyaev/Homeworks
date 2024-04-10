95d6acfe86f70d312e03c98b286218a374cc80e1
MuradYagyaev <165575932+MuradYagyaev@users.noreply.github.com>
2024-04-10 21:27:49 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+# Модуль 1 урок №4. "Организация программ и методы строк."
+my_string = input()
+# print("Длина строки:", len(my_string), "символов")
+print("Длина строки:", my_string.__len__(), "символов")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
